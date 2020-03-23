b80d6557ecf1bf11f870b0a88a1a8a216a84f39c
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-23 17:48:04 +0100

    test: fix snapshots being considered test files

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
 	testEnvironment: 'node',
 	transform: defaults.transform,
-	testRegex: `test/.*\.test\.ts`,
+	testRegex: `test/.*\.test\.ts$`,
 	globals: {
 		'ts-jest': {
 			packageJson: path.join(__dirname, 'package.json')
