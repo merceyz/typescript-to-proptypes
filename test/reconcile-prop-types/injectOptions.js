3659370f6bae8cc1bfa30153245c1d03c017e5d5
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-27 12:52:31 +0100

    feat(injector): Add reconcilePropTypes

--- test/reconcile-prop-types/injectOptions.js
+++ test/reconcile-prop-types/injectOptions.js
@@ -1,3 +1,11 @@
 module.exports = {
 	removeExistingPropTypes: true,
+	reconcilePropTypes: (prop, previous, generated) => {
+		const isCustomValidator = !previous.startsWith('PropTypes');
+
+		if (isCustomValidator) {
+			return previous;
+		}
+		return generated;
+	},
 };
--- test/reconcile-prop-types/output.js
+++ test/reconcile-prop-types/output.js
@@ -12,7 +12,18 @@ function Component(props) {
 }
 
 Component.propTypes = {
-	children: PropTypes.node,
+	children: chainPropTypes(PropTypes.node.isRequired, (props) => {
+		const summary = React.Children.toArray(props.children)[0];
+		if (isFragment(summary)) {
+			return new Error('Not accepting Fragments');
+		}
+
+		if (!React.isValidElement(summary)) {
+			return new Error('First child must be an element');
+		}
+
+		return null;
+	}),
 };
 
 export default Component;
